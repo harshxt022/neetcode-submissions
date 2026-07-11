d14bace8d825652b2f533174a70a6c0f66292020
Harshit Hassija <harshithassija022@gmail.com>
2026-07-11 23:49:35 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int j = s.length()-1;
+        int i = 0;
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                if(isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1)){
+                    return true;
+                }else return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static boolean isPalindrome(String s,int i,int j){
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
