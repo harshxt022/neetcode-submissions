7c65c7dfcb9aa82eb0d41f65cd17c8b73224a9d6
Harshit Hassija <harshithassija022@gmail.com>
2026-07-13 10:40:54 +0530

    Add: crawler-log-folder - submission-2

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        String out = "../";
+        String same = "./";
+        int count = 0;
+        for(int i = 0;i<logs.length;i++){
+            if(logs[i].equals(same)) continue;
+            else if(logs[i].equals(out)){
+                if (count > 0) count--;
+            }else count++;
+        }
+        return count;
+    }
+}
