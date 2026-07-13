ba677e8bbdee019e959f970afa03debd8f529472
Harshit Hassija <harshithassija022@gmail.com>
2026-07-13 11:46:34 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0;i<operations.length;i++){
+            if(operations[i].equals("+")){
+                // s.push(operations[i-1]+operations[i-2]);
+                int temp = s.pop();
+                int total = s.peek()+ temp;
+                s.push(temp);
+                s.push(total);
+            }
+            else if(operations[i].equals("D")){
+                s.push(2*s.peek());
+            }
+            else if(operations[i].equals("C")){
+                s.pop();
+            }else{
+                s.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int count = 0;
+        while(!s.isEmpty()){
+            count+=s.pop();
+        }
+        return count;
+    }
+}
